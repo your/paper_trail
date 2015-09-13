9ae21991a26ce618b089654f581c2b61973ca763
Jared Beck <jared@jaredbeck.com>
2015-09-13 13:31:21 -0400

    Clarify assocations tests
    - Improve descriptions
    - Decrease context nesting
    - Prefer local variables over instance variables
    - Name version variables more consistently
      - Use version numbers instead of `last`

--- test/unit/associations_test.rb
+++ test/unit/associations_test.rb
@@ -20,142 +20,143 @@ class AssociationsTest < ActiveSupport::TestCase
     context "before any associations are created" do
       setup do
         @chapter.update_attributes(:name => "ch_2")
-        @ch_1 = @chapter.versions.last.reify(:has_many => true)
       end
 
-      should "reify the record when reify is called" do
-        assert_equal "ch_1", @ch_1.name
+      should "not reify any associations" do
+        chapter_v1 = @chapter.versions[1].reify(:has_many => true)
+        assert_equal "ch_1", chapter_v1.name
+        assert_equal [], chapter_v1.sections
+        assert_equal [], chapter_v1.paragraphs
       end
     end
 
     context "after the first has_many through relationship is created" do
       setup do
+        assert_equal 1, @chapter.versions.size
+
         Timecop.travel 1.second.since
         @chapter.update_attributes :name => "ch_2"
+        assert_equal 2, @chapter.versions.size
+
         Timecop.travel 1.second.since
         @chapter.sections.create :name => "section 1"
         Timecop.travel 1.second.since
         @chapter.sections.first.update_attributes :name => "section 2"
         Timecop.travel 1.second.since
         @chapter.update_attributes :name => "ch_3"
+        assert_equal 3, @chapter.versions.size
+
         Timecop.travel 1.second.since
         @chapter.sections.first.update_attributes :name => "section 3"
       end
 
-      context "when reify is called" do
-        setup do
-          @chapter_1 = @chapter.versions.last.reify(:has_many => true)
-        end
-
-        should "show the value of the base record as it was before" do
-          assert_equal "ch_2", @chapter_1.name
-        end
-
-        should "show the value of the associated record as it was before the base record was updated" do
-          assert_equal ['section 2'], @chapter_1.sections.map(&:name)
-        end
-
-        context "to the version before the relationship was created" do
-          setup { @chapter_2 = @chapter.versions.second.reify(:has_many => true) }
-
-          should "not return any associated records" do
-            assert_equal 0, @chapter_2.sections.size
-          end
-        end
-
-        context "to the version before the associated record has been destroyed" do
-          setup do
-            @chapter.update_attributes :name => 'ch_3'
-            Timecop.travel 1.second.since
-            @chapter.sections.destroy_all
-            Timecop.travel 1.second.since
-            @chapter_3 = @chapter.versions.last.reify(:has_many => true)
-          end
-
-          should "return the associated record" do
-            assert_equal ['section 2'], @chapter_3.sections.map(&:name)
-          end
-        end
-
-        context "to the version after the associated record has been destroyed" do
-          setup do
-            @chapter.sections.destroy_all
-            Timecop.travel 1.second.since
-            @chapter.update_attributes :name => 'ch_4'
-            Timecop.travel 1.second.since
-            @chapter_4 = @chapter.versions.last.reify(:has_many => true)
-          end
-
-          should "return the associated record" do
-            assert_equal 0, @chapter_4.sections.size
-          end
+      context "version 1" do
+        should "have no sections" do
+          chapter_v1 = @chapter.versions[1].reify(:has_many => true)
+          assert_equal [], chapter_v1.sections
         end
       end
 
-      context "after the nested has_many through relationship is created" do
+      context "version 2" do
+        should "have one section" do
+          chapter_v2 = @chapter.versions[2].reify(:has_many => true)
+          assert_equal 1, chapter_v2.sections.size
+
+          # Shows the value of the section as it was before
+          # the chapter was updated.
+          assert_equal ['section 2'], chapter_v2.sections.map(&:name)
+
+          # Shows the value of the chapter as it was before
+          assert_equal "ch_2", chapter_v2.name
+        end
+      end
+
+      context "version 2, before the section was destroyed" do
         setup do
+          @chapter.update_attributes :name => 'ch_3'
+          Timecop.travel 1.second.since
+          @chapter.sections.destroy_all
+          Timecop.travel 1.second.since
+        end
+
+        should "have the one section" do
+          chapter_v2 = @chapter.versions[2].reify(:has_many => true)
+          assert_equal ['section 2'], chapter_v2.sections.map(&:name)
+        end
+      end
+
+      context "version 3, after the section was destroyed" do
+        setup do
+          @chapter.sections.destroy_all
+          Timecop.travel 1.second.since
+          @chapter.update_attributes :name => 'ch_4'
+          Timecop.travel 1.second.since
+        end
+
+        should "have no sections" do
+          chapter_v3 = @chapter.versions[3].reify(:has_many => true)
+          assert_equal 0, chapter_v3.sections.size
+        end
+      end
+
+      context "after creating a paragraph" do
+        setup do
+          assert_equal 3, @chapter.versions.size
           @section = @chapter.sections.first
           @paragraph = @section.paragraphs.create :name => 'para1'
         end
 
-        context "reify the associations" do
-          setup do
+        context "new chapter version" do
+          should "have one paragraph" do
+            initial_section_name = @section.name
+            initial_paragraph_name = @paragraph.name
             Timecop.travel 1.second.since
-            @initial_section_name = @section.name
-            @initial_paragraph_name = @paragraph.name
             @chapter.update_attributes :name => 'ch_5'
+            assert_equal 4, @chapter.versions.size
             Timecop.travel 1.second.since
             @paragraph.update_attributes :name => 'para3'
-            Timecop.travel 1.second.since
-            @chapter_4 = @chapter.versions.last.reify(:has_many => true)
-          end
-
-          should "to the version before the change was made" do
-            assert_equal [@initial_section_name], @chapter_4.sections.map(&:name)
-            assert_equal [@initial_paragraph_name], @chapter_4.sections.first.paragraphs.map(&:name)
+            chapter_v3 = @chapter.versions[3].reify(:has_many => true)
+            assert_equal [initial_section_name], chapter_v3.sections.map(&:name)
+            paragraphs = chapter_v3.sections.first.paragraphs
+            assert_equal 1, paragraphs.size
+            assert_equal [initial_paragraph_name], paragraphs.map(&:name)
           end
         end
 
-        context "and the first has_many through relationship is destroyed" do
-          setup do
+        context "destroy a section" do
+          should "not have any sections or paragraphs" do
             @section.destroy
             Timecop.travel 1.second.since
             @chapter.update_attributes(:name => 'chapter 6')
-            Timecop.travel 1.second.since
-            @chapter_before = @chapter.versions.last.reify(:has_many => true)
-          end
-
-          should "reify should not return any associated models" do
-            assert_equal 0, @chapter_before.sections.size
-            assert_equal 0, @chapter_before.paragraphs.size
+            assert_equal 4, @chapter.versions.size
+            chapter_v3 = @chapter.versions[3].reify(:has_many => true)
+            assert_equal 0, chapter_v3.sections.size
+            assert_equal 0, chapter_v3.paragraphs.size
           end
         end
 
-        context "reified to the version before the nested has_many through relationship is destroyed" do
-          setup do
+        context "the version before a paragraph is destroyed" do
+          should "have the one paragraph" do
+            initial_paragraph_name = @section.paragraphs.first.name
             Timecop.travel 1.second.since
-            @initial_paragraph_name = @section.paragraphs.first.name
             @chapter.update_attributes(:name => 'chapter 6')
             Timecop.travel 1.second.since
             @paragraph.destroy
-            @chapter_before = @chapter.versions.last.reify(:has_many => true)
-          end
-
-          should "restore the associated has_many relationship" do
-            assert_equal [@initial_paragraph_name], @chapter_before.sections.first.paragraphs.map(&:name)
+            chapter_v3 = @chapter.versions[3].reify(:has_many => true)
+            paragraphs = chapter_v3.sections.first.paragraphs
+            assert_equal 1, paragraphs.size
+            assert_equal initial_paragraph_name, paragraphs.first.name
           end
         end
 
-        context "reified to the version after the nested has_many through relationship is destroyed" do
-          setup do
+        context "the version after a paragraph is destroyed" do
+          should "have no paragraphs" do
             @paragraph.destroy
             Timecop.travel 1.second.since
             @chapter.update_attributes(:name => 'chapter 6')
-            @chapter_before = @chapter.versions.last.reify(:has_many => true)
-          end
-
-          should "restore the associated has_many relationship" do
-            assert_equal [], @chapter_before.sections.first.paragraphs
+            chapter_v3 = @chapter.versions[3].reify(:has_many => true)
+            assert_equal 0, chapter_v3.paragraphs.size
+            assert_equal [], chapter_v3.sections.first.paragraphs
           end
         end
       end
